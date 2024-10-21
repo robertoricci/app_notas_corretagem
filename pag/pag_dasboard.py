c5e3137ec5e3ad3a62725916a3c040f3530d0fc8
Roberto Ricci <roberto.ricci@sonepar.com.br>
2024-10-21 12:39:13 -0300

    teste retirado a main

--- pag/pag_dasboard.py
+++ pag/pag_dasboard.py
@@ -50,140 +50,140 @@ def main():
    
 
 
-    if (df.size  > 0):
+    # if (df.size  > 0):
 
-        df['data_de_pregao'] = pd.to_datetime(df['data_de_pregao']).dt.date
+    #     df['data_de_pregao'] = pd.to_datetime(df['data_de_pregao']).dt.date
 
-        dt_min = df["data_de_pregao"].min()
-        dt_max = df["data_de_pregao"].max()
+    #     dt_min = df["data_de_pregao"].min()
+    #     dt_max = df["data_de_pregao"].max()
 
 
-        data_selecionada = st.date_input("Selecione o intervalo de datas:",value=[dt_min, dt_max],min_value=dt_min,max_value=dt_max)
+    #     data_selecionada = st.date_input("Selecione o intervalo de datas:",value=[dt_min, dt_max],min_value=dt_min,max_value=dt_max)
         
-        try:
-            data_ini_sel =  data_selecionada[0]
-        except:
-            data_ini_sel =  dt_min
+    #     try:
+    #         data_ini_sel =  data_selecionada[0]
+    #     except:
+    #         data_ini_sel =  dt_min
 
-        try:
-            data_fim_sel =  data_selecionada[1]
-        except:
-            data_fim_sel =  dt_max
+    #     try:
+    #         data_fim_sel =  data_selecionada[1]
+    #     except:
+    #         data_fim_sel =  dt_max
 
-        df_filtered = df[df["data_de_pregao"].between(data_ini_sel, data_fim_sel)]
+    #     df_filtered = df[df["data_de_pregao"].between(data_ini_sel, data_fim_sel)]
         
         
-        corretora_selecionada = st.multiselect("Selecione a Corretora:",options=df_filtered["corretora"].unique(),  default=df_filtered["corretora"].unique())
-        ticker_selecionado = st.multiselect("Selecione o Ticker:",options=df_filtered["ticker"].unique(),  default=df_filtered["ticker"].unique())
+    #     corretora_selecionada = st.multiselect("Selecione a Corretora:",options=df_filtered["corretora"].unique(),  default=df_filtered["corretora"].unique())
+    #     ticker_selecionado = st.multiselect("Selecione o Ticker:",options=df_filtered["ticker"].unique(),  default=df_filtered["ticker"].unique())
 
-        df_filtered = df_filtered[(df_filtered["corretora"].isin(corretora_selecionada)) & 
-                    (df_filtered["ticker"].isin(ticker_selecionado)) 
-                    ]
+    #     df_filtered = df_filtered[(df_filtered["corretora"].isin(corretora_selecionada)) & 
+    #                 (df_filtered["ticker"].isin(ticker_selecionado)) 
+    #                 ]
         
-        df_mov = df_filtered.groupby('cv')['movimentacao'].sum().reset_index()
+    #     df_mov = df_filtered.groupby('cv')['movimentacao'].sum().reset_index()
 
-        col1,col2,col3,col4 = st.columns([3,3,3,3])
+    #     col1,col2,col3,col4 = st.columns([3,3,3,3])
     
-        with col1:
-            st.html('<span class="Medio_indicator"></span>')
-            st.metric(label='Total Tickers', value=len(df_filtered['ticker'].unique()))
+    #     with col1:
+    #         st.html('<span class="Medio_indicator"></span>')
+    #         st.metric(label='Total Tickers', value=len(df_filtered['ticker'].unique()))
 
-        with col2:
-            st.html('<span class="Medio_indicator"></span>')
-            compra = df_mov[df_mov['cv']=='Compra'].values[0][1]
-            st.metric(label='Compra', value=f"R$ {compra:,.2f}".replace(",", "X").replace(".", ",").replace("X", "."))
+    #     with col2:
+    #         st.html('<span class="Medio_indicator"></span>')
+    #         compra = df_mov[df_mov['cv']=='Compra'].values[0][1]
+    #         st.metric(label='Compra', value=f"R$ {compra:,.2f}".replace(",", "X").replace(".", ",").replace("X", "."))
 
-        with col3:
-            st.html('<span class="Medio_indicator"></span>')
-            venda = df_mov[df_mov['cv']=='Venda'].values[0][1]
-            st.metric(label='Venda', value=f"R$ {venda:,.2f}".replace(",", "X").replace(".", ",").replace("X", "."))
+    #     with col3:
+    #         st.html('<span class="Medio_indicator"></span>')
+    #         venda = df_mov[df_mov['cv']=='Venda'].values[0][1]
+    #         st.metric(label='Venda', value=f"R$ {venda:,.2f}".replace(",", "X").replace(".", ",").replace("X", "."))
 
-        with col4:
-            st.html('<span class="Medio_indicator"></span>')
-            saldo =  venda - abs(compra)
-            st.metric(label='Saldo', value=f"R$ {saldo:,.2f}".replace(",", "X").replace(".", ",").replace("X", "."))
+    #     with col4:
+    #         st.html('<span class="Medio_indicator"></span>')
+    #         saldo =  venda - abs(compra)
+    #         st.metric(label='Saldo', value=f"R$ {saldo:,.2f}".replace(",", "X").replace(".", ",").replace("X", "."))
 
-        df_soma = df_filtered.groupby('ticker')['qted'].sum()
+    #     df_soma = df_filtered.groupby('ticker')['qted'].sum()
 
-        df_soma = df_soma.to_frame()
+    #     df_soma = df_soma.to_frame()
 
-        df_tickers = montar_dataframe_ticker(df_soma)
+    #     df_tickers = montar_dataframe_ticker(df_soma)
         
-        st.markdown('<h3 class="subtitulo">Portifólio</h3>', unsafe_allow_html=True)
+    #     st.markdown('<h3 class="subtitulo">Portifólio</h3>', unsafe_allow_html=True)
 
-        st.dataframe(df_tickers,hide_index= False, column_config={"image": st.column_config.ImageColumn(help='IMag'), 
-                                                }
-                                ,use_container_width=False)
+    #     st.dataframe(df_tickers,hide_index= False, column_config={"image": st.column_config.ImageColumn(help='IMag'), 
+    #                                             }
+    #                             ,use_container_width=False)
 
-        st.write('')
-        st.write('')
+    #     st.write('')
+    #     st.write('')
     
-        st.markdown('<h3 class="subtitulo">Extrato</h3>', unsafe_allow_html=True)
-        st.dataframe(df_filtered,use_container_width=True)
-
-    
-        st.markdown('<h3 class="subtitulo">Quantidade por ticker</h3>', unsafe_allow_html=True)
-        qted_chart = alt.Chart(df_filtered).mark_bar().encode(
-        x='ticker:N',
-        y='qted:Q',
-        color='ticker:N'
-        ).properties(
-        title=''
-        )
-        st.altair_chart(qted_chart, use_container_width=True)
-
-
-        # st.header("Movimentação ao longo do tempo")
-        # movimentacao_chart = alt.Chart(df_filtered).mark_line().encode(
-        #     x='data_de_pregao:T',
-        #     y='movimentacao:Q',
-        #     color='ticker:N'
-        # ).properties(
-        #     title='Movimentação por Mercadoria ao Longo do Tempo'
-        # )
-        # st.altair_chart(movimentacao_chart, use_container_width=True)  
-
-        df_agrupado = df_filtered.groupby(['data_de_pregao'])['movimentacao'].sum().reset_index()
-
-        ##df_agrupado.set_index('data_de_pregao',inplace=True)
-
-
-        ##df_filtered.set_index('data_de_pregao',inplace=True)
-
-        ##st.dataframe(df_agrupado)
-
-        import plotly.express as px
-
-
-        # fig = px.line(df_agrupado, 
-        #             x='data_de_pregao', 
-        #             y='movimentacao', 
-        #             color='ticker', 
-        #             title='Movimentação por Data e Ticker')
-
-        # # Exibir o gráfico no Streamlit
-        # st.plotly_chart(fig)
+    #     st.markdown('<h3 class="subtitulo">Extrato</h3>', unsafe_allow_html=True)
+    #     st.dataframe(df_filtered,use_container_width=True)
 
     
-        df_agrupado['data_de_pregao'] = pd.to_datetime(df_agrupado['data_de_pregao'])
+    #     st.markdown('<h3 class="subtitulo">Quantidade por ticker</h3>', unsafe_allow_html=True)
+    #     qted_chart = alt.Chart(df_filtered).mark_bar().encode(
+    #     x='ticker:N',
+    #     y='qted:Q',
+    #     color='ticker:N'
+    #     ).properties(
+    #     title=''
+    #     )
+    #     st.altair_chart(qted_chart, use_container_width=True)
+
+
+    #     # st.header("Movimentação ao longo do tempo")
+    #     # movimentacao_chart = alt.Chart(df_filtered).mark_line().encode(
+    #     #     x='data_de_pregao:T',
+    #     #     y='movimentacao:Q',
+    #     #     color='ticker:N'
+    #     # ).properties(
+    #     #     title='Movimentação por Mercadoria ao Longo do Tempo'
+    #     # )
+    #     # st.altair_chart(movimentacao_chart, use_container_width=True)  
+
+    #     df_agrupado = df_filtered.groupby(['data_de_pregao'])['movimentacao'].sum().reset_index()
+
+    #     ##df_agrupado.set_index('data_de_pregao',inplace=True)
+
+
+    #     ##df_filtered.set_index('data_de_pregao',inplace=True)
+
+    #     ##st.dataframe(df_agrupado)
+
+    #     import plotly.express as px
+
+
+    #     # fig = px.line(df_agrupado, 
+    #     #             x='data_de_pregao', 
+    #     #             y='movimentacao', 
+    #     #             color='ticker', 
+    #     #             title='Movimentação por Data e Ticker')
+
+    #     # # Exibir o gráfico no Streamlit
+    #     # st.plotly_chart(fig)
+
     
-        fig = px.line(df_agrupado, x='data_de_pregao', y='movimentacao', title='')
+    #     df_agrupado['data_de_pregao'] = pd.to_datetime(df_agrupado['data_de_pregao'])
+    
+    #     fig = px.line(df_agrupado, x='data_de_pregao', y='movimentacao', title='')
         
-        st.markdown('<h3 class="subtitulo">Movimentação ao Longo do Tempo</h3>', unsafe_allow_html=True)
-        st.plotly_chart(fig)
+    #     st.markdown('<h3 class="subtitulo">Movimentação ao Longo do Tempo</h3>', unsafe_allow_html=True)
+    #     st.plotly_chart(fig)
 
-        df_sum = df_filtered.groupby('ticker')['movimentacao'].sum().reset_index()
+    #     df_sum = df_filtered.groupby('ticker')['movimentacao'].sum().reset_index()
 
-        # Criar o gráfico de barras
-        fig = px.bar(df_sum, 
-                    x='ticker', 
-                    y='movimentacao', 
-                    color='ticker', 
-                    title='')
+    #     # Criar o gráfico de barras
+    #     fig = px.bar(df_sum, 
+    #                 x='ticker', 
+    #                 y='movimentacao', 
+    #                 color='ticker', 
+    #                 title='')
 
     
-        st.markdown('<h3 class="subtitulo">Movimentação port Ticker</h3>', unsafe_allow_html=True)
-        st.plotly_chart(fig)                      
+    #     st.markdown('<h3 class="subtitulo">Movimentação port Ticker</h3>', unsafe_allow_html=True)
+    #     st.plotly_chart(fig)                      
 
 
 
